80015fc28cc32a45adff65d696b2cad3e8f7498a
aarizaq <aarizaq_m@hotmail.com>
2009-09-30 17:39:03 +0200

    Disable the Aodv debug messages for eficiency

--- src/networklayer/manetrouting/aodv/aodv-uu/debug_aodv.h
+++ src/networklayer/manetrouting/aodv/aodv-uu/debug_aodv.h
@@ -70,10 +70,10 @@ const char *state_to_str(u_int8_t state);
 #endif
 */
 
-//#ifdef DEBUG
-//#undef DEBUG
-//#endif
-#define DEBUG
+#ifdef DEBUG
+#undef DEBUG
+#endif
+//#define DEBUG
 
 #ifdef DEBUG
 #undef DEBUG
