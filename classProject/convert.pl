1b784a4378ad50d6e848b88b6ff5bae35ade07ca
Eric Norige <thelema314@gmail.com>
2009-12-14 15:53:07 -0500

    process tcp level data

--- classProject/convert.pl
+++ classProject/convert.pl
@@ -29,6 +29,16 @@ foreach my $sca (@ARGV) {
 	    $data{$2}{$1}[$i]=$3;
 	} elsif (/scalar \w+.(\w+).+\s+TRSS\s+(\d+)/) {
 	    $nodes{$1}{cost} = $2;
+	} elsif (/scalar \w+.configurator\s+(\w+)\s+(\d+)/) {
+	    $ip1 = $2>>24; $ip2 = ($2 & 0xFFFFFF) >>16; 
+	    $ip3 = ($2 & 0xFFFF)>>8; $ip4=$2 & 0xFF;
+	    $ip = "$ip1.$ip2.$ip3.$ip4";
+	    $by_ip{$ip} = $1;
+	} elsif (/scalar \w+.(\w+).networkLayer.ip\s+([0-9\.]+)\s+(\d+)/) {
+	    $src = $by_ip{$2};
+	    $ipdata{$src}{$1}[$i]=$3;
+	    $w = log (1+$3) / (log 2) - 14;
+#	    print "  $src -> $1 [color=blue penwidth=\"$w\"];\n";
 	}
     }
     close SCA;
@@ -52,16 +62,17 @@ foreach my $src (keys %data) {
 
 	push @edges, {diff => $d0, src => $src, dst => $dst};
 	push @edges, {diff => $d1, src => $dst, dst => $src};
-	$min = $d0 if abs $d0 < $min;
-	$max = $d0 if abs $d0 > $max;
-	$min = $d1 if abs $d1 < $min;
-	$max = $d1 if abs $d1 > $max;
+	$d0 = abs $d0; $d1 = abs $d1;
+	$min = $d0 if $d0 < $min;
+	$max = $d0 if $d0 > $max;
+	$min = $d1 if $d1 < $min;
+	$max = $d1 if $d1 > $max;
     }
 }
 
 foreach my $e (@edges) {
     my $color = $e->{diff} > 0 ? "red" : "green";
-    my $w = (log abs $e->{diff} - log abs $max) / log 2;
+    my $w = (log abs $e->{diff}+1 - log $min+1) / log 2;
     print "  $e->{src} -> $e->{dst} [color=$color penwidth=\"$w\"];\n";
 }
 
